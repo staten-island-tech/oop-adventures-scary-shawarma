0ba6824ce047c236e2752977f5c9451eb5372478
1hyphend <Ivanc221@nycstudents.net>
2026-01-07 10:16:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,50 @@
-class nun_player_character():
-    def nun_player_character(self, name, lvlreq):
-        self.name = name 
-        self.lvlrq = lvlreq
-        self.
+import tkinter as tk
+window = tk.Tk()
+window.title("Welcome to scary adventure game")
+window.geometry("700x600")
+question_label = tk.Label(window, text="", wraplength=550, font=("Sans serif", 14))
+question_label.pack(pady=20)
+buttons = []
+window.title("This is crackheads shop")
+title = tk.Label(window, text="Welcome to crackheads shop", font=("Arial",16))
+for i in range(8):
+    button = tk.Button(window, text="", width=40, font=("Sans serif", 12))
+    button.pack(pady=5)
+    buttons.append(button)
+buttons[0]
+buttons[1]
+buttons[2]
+buttons[3]
+buttons[4]
+buttons[5]
+buttons[6]
+buttons[7]
+# class nun_player_character():
+#     def nun_player_character(self, name, lvlreq, reward, quest):
+#         self.name = name 
+#         self.lvlrq = lvlreq
+#         self.reward = reward
+#         self.quest = quest 
+#         def name():
+#             if mc self.lvlreq   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
