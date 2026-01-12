0f6a2cc8165264de55ff4af1df59cf232f5ff3cd
a <justinc1127@nycstudents.net>
2026-01-12 09:59:17 -0500

    Update test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+class hero():
+    def mc(self,name,stamina,hp,attack,level,money):
+        self.name=name
+        self.stamina=stamina
+        self.hp=hp
+        self.attack=attack
+        self.money = money
+        money=100
+    global attack
+    global stamina
+    global hp
+    stamina=100
+    hp=100
+    attack=25
+# justin = MC(100,100,100,100,100,100,3,1
+    name=input("Give me a name plz")
+    def potion(self,strength,health,stamina):
+        self.strength=strength
+        self.health=health
+        self.stamina=stamina
+        strength=attack*1.5
+        stamina=1.5*stamina
+    def health(self,regeneration,point):
+        self.regeneration=regeneration
+        self.point=point
+        point=3
+        regeneration=1
+    global point
+    global regeneration
+    def sword_stat(self,damage):
+        self.damage=damage
+    
